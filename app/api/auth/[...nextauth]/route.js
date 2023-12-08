ce6e4aa46e03695708625a8be21688805b4c6cb9
David Okaliwe <okaliwedavid@gmail.com>
2023-12-08 15:51:43 +0100

    Updates

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { Providers } from "next-auth";
+import Providers from 'next-auth/providers'
 import connectToDb from "@utils/database";
 import Admin from "@models/admin";
 
