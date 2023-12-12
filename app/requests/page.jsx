929f189f811eacd04b299037513538b2700af995
David Okaliwe <okaliwedavid@gmail.com>
2023-12-13 00:15:54 +0100

    Updates

--- app/requests/page.jsx
+++ app/requests/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const RequestsPage = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (!session) {
+    router.replace("/");
+    return null;
+  }
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h1 className="text-white">Requests Page</h1>
+      {/* Your content for authenticated users */}
+    </div>
+  );
+};
 
-export default page
+export default RequestsPage;
