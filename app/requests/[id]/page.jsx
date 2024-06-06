febb89d5bd696c4556badb046eb77ef620dbfa49
David Okaliwe <okaliwedavid@gmail.com>
2024-06-06 18:07:41 +0100

    Updates

--- app/requests/[id]/page.jsx
+++ app/requests/[id]/page.jsx
@@ -36,8 +36,8 @@ const UserDetails = ({ params }) => {
 
   return (
     <section className="md:min-h-[800px] ss:min-h-[620px] min-h-[650px] 
-    flex items-center md:px-16 px-6 mt-24">
-      <div className="items-center w-full flex flex-col md:gap-8 
+    flex items-center md:px-16 px-6 mt-48">
+      <div className="items-center w-full flex flex-col md:gap-10 
       font-manierRegular">
         <div className='w-full flex justify-between items-center'>
           <div className='flex flex-col gap-2'>
@@ -73,20 +73,160 @@ const UserDetails = ({ params }) => {
           </h1>
         </div>
         
-        <div className="mt-4">
-          <div><strong>Email Address:</strong> {userData.email}</div>
-          <div><strong>Phone Number:</strong> {userData.phoneNumber}</div>
-          <div><strong>Date of Birth:</strong> {userData.dob}</div>
-          <div><strong>Gender:</strong> {userData.gender}</div>
-          <div><strong>Employer:</strong> {userData.employer}</div>
-          <div><strong>Occupation:</strong> {userData.occupation}</div>
-          <div><strong>Twitter Handle:</strong> {userData.twitterHandle}</div>
-          <div><strong>Instagram Handle:</strong> {userData.instagramHandle}</div>
-          <div><strong>Facebook Handle:</strong> {userData.facebookHandle}</div>
-          <div><strong>What turns you on the most?</strong> {userData.turnOn}</div>
-          <div><strong>If you could amplify an erotic trait within yourself, what would it be?</strong> {userData.eroticTrait}</div>
-          <div><strong>What would you contribute to the Nuude! community?</strong> {userData.contribution}</div>
-          <div><strong>And finally, how did you find us?</strong> {userData.referral}</div>
+        <div className="w-full grid grid-cols-2 md:gap-6 ss:gap-6 gap-4">
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+          
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <p className='text-textalt md:text-[16px] ss:text-16px]
+            text-[13px]'>
+              Full Name
+            </p>
+            <p className='text-white md:text-[17px] ss:text-17px]
+            text-[14px]'>
+              {userData.firstName} {userData.lastName}
+            </p>
+          </div>
       </div>
     </div>
     </section>
