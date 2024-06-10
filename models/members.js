5d3993f8bf7190cbfee84b41adc5068a10733eff
David Okaliwe <okaliwedavid@gmail.com>
2024-06-10 12:00:52 +0100

    Updates

--- models/members.js
+++ models/members.js
@@ -4,8 +4,6 @@ const MemberSchema = new Schema(
     {
         userId: {
             type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            unique: true,
         },
         firstName: {
             type: String,
@@ -15,12 +13,64 @@ const MemberSchema = new Schema(
             type: String,
             required: true,
         },
-        paymentType: {
+        email: {
             type: String,
-            enum: ['Annual Membership'],
             required: true,
         },
-        email: {
+        phone: {
+            type: String,
+            required: true,
+        },
+        birthdate: {
+            type: String,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+        },
+        employer: {
+            type: String,
+            required: true,
+        },
+        occupation: {
+            type: String,
+            required: true,
+        },
+        instagram: {
+            type: String,
+            required: true,
+        },
+        twitter: {
+            type: String,
+            required: true,
+        },
+        facebook: {
+            type: String,
+            required: true,
+        },
+        turnons: {
+            type: String,
+            required: true,
+        },
+        trait: {
+            type: String,
+            required: true,
+        },
+        contribution: {
+            type: String,
+            required: true,
+        },
+        mode: {
+            type: String,
+            required: true,
+        },
+        paymentType: {
+            type: String,
+            enum: ['Guest Ticket', 'Membership'],
+            required: true,
+        },
+        reference: {
             type: String,
             required: true,
         },
